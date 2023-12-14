4ecc66e7291071d2942594e75dcf35959fb2301a
Abdulloh9090 <152011956+Abdulloh9090@users.noreply.github.com>
2023-12-14 18:45:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+ // const Nuriddin = 'Nuriddin'
+
+ // console.log(Nuriddin);
+
+ // const Nuriddinaka = '👍'
+
+ // console.log(Nuriddinaka);
+
+
+
+
+ // let ism = '40'
+ // let ism2 = '50'
+ // console.log(ism == ism2);
+
+
+// console.log(776 % 2);
+
+
+
+
+// let ism = prompt('Ismingiz')
+// let ism = prompt('Familiya')
+// console.log(ism);
+// alert(ism)
+
+
+
+
+
+ let ism = prompt("Ismingizni kiriting:", "Abdulloh");
+    ism2 = prompt("Familyangizni kiriting:", "Mirasqarov")
+ alert("Ismingiz: " + ism  );
+alert("Familyangiz: "  +ism2)
+ console.log(ism);
+ console.log(ism2);
+
+
+
+
+
+
+
+
+
+
+
